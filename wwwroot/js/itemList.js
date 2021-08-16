d78cb26c72c1709d7682165b91287c8c96dbcfe1
AbdalrhmanDev <a.m.a.alkhulaqi@gmail.com>
2021-08-16 15:59:04 +0300

    some files

---
+++ wwwroot/js/itemList.js
@@ -0,0 +1,76 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadTable();
+});
+
+
+function loadTable() {
+    dataTable = $("#DT_load").DataTable({
+       // "processing": true,
+       // "serverSide": true,
+        "ajax": {
+            "url": "/Items/getall",
+            "type": "GET",
+            "datatype": "json",
+            "error": function (e) { },
+            "dataSrc": function (d) {
+                return d;
+            }
+        },
+        "order": [[0, "asc"]],
+
+        "stateSave": "true",
+       
+        "columns": [
+            { "data": "name", "width": "80%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center btn-group" style="direction:ltr">
+                                <a onclick=Delete('/Items/Delete?id=${data}') class="btn btn-oval btn-danger text-withe" style="cursor:pointer; width:70px;">
+                                 حذف
+                                    </a>
+                               
+                                <a href="/Items/Upsert?id=${data}" class="btn btn-oval btn-info text-withe" style="cursor:pointer; width:70px;">
+                                تعديل
+                                    </a>
+                                </div>`;
+                }, "width": "20%"
+            }
+        ],
+       
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+   
+}
+function Delete(url) {
+    swal({
+        title: "تأكيد الحذف",
+        text: "هل أنت متأكد من إجراء عملية الحذف؟",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                       // toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
+
+
---
+++ wwwroot/js/orderList.js
@@ -0,0 +1,78 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadTable();
+});
+
+
+function loadTable() {
+    dataTable = $("#DT_load").DataTable({
+       // "processing": true,
+       // "serverSide": true,
+        "ajax": {
+            "url": "/Orders/getall",
+            "type": "GET",
+            "datatype": "json",
+            "error": function (e) { },
+            "dataSrc": function (d) {
+                return d;
+            }
+        },
+        "order": [[0, "asc"]],
+
+        "stateSave": "true",
+       
+        "columns": [
+            { "data": "userName", "width": "40%" },
+            { "data": "date", "width": "30%" },
+            { "data": "adminAproval", "width": "10%" },
+            { "data": "isSend", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center btn-group" style="direction:ltr">
+                                <a onclick=Delete('/Orders/Delete?id=${data}') class="btn btn-oval btn-danger text-withe" style="cursor:pointer; width:70px;">
+                                 حذف
+                                    </a>
+                                <a href="/Orders/Upsert?id=${data}" class="btn btn-oval btn-info text-withe" style="cursor:pointer; width:70px;">
+                                تعديل
+                                    </a>
+                                </div>`;
+                }, "width": "10%"
+            }
+        ],
+       
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+   
+}
+function Delete(url) {
+    swal({
+        title: "تأكيد الحذف",
+        text: "هل أنت متأكد من إجراء عملية الحذف؟",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
+
+
